598e1710651f723aa90cf552c8ec9ece5adf6ab5
Kevin <kevinum2424@gmail.com>
2020-02-15 19:28:17 +0000

    first commit

---
+++ lab2/program1.c
@@ -0,0 +1,12 @@
+/* My first Raspberry Pi Program
+   Name: Kevin Um
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello world.\n");
+	printf("Compiled and run on a Raspberry Pi.\n");
+	return 0;
+}

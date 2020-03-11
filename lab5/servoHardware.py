28655b5564576ef4fd31c0963ffef930bfedbbd3
Kevin <kevinum2424@gmail.com>
2020-03-11 23:59:45 +0000

    revised lab5

--- lab5/servoHardware.py
+++ lab5/servoHardware.py
@@ -13,6 +13,12 @@ MOTOR = 18 # Connect servomotor to BCM 18
 BUTTON = 12 # Connect button to BCM 12
 current_state = 'WAIT_FOR_BUTTON' # Program should begin with waiting for button press
 
+MAX_PULSEWIDTH = 2000
+MIN_PULSEWIDTH = 1000
+PULSEWIDTH_0DEGREES = 1500
+ANGLE_OF_ROTATION = 180
+
+# --------------------- GIVEN ---------------------
 pi = pigpio.pi()
 if not pi.connected:
     exit(0)
@@ -29,7 +35,7 @@ def move_to_angle(pulseWidth):
     """
     pulseWidth ranges from 1000 to 2000
     """
-    if (1000 <= pulseWidth <= 2000):
+    if (MIN_PULSEWIDTH <= pulseWidth <= MAX_PULSEWIDTH):
         print"Servo motor pulse width: ", pulseWidth
         # print"Given servo motor angle: ", degrees, "degrees"
         pi.set_servo_pulsewidth(MOTOR, pulseWidth)
@@ -43,7 +49,7 @@ def to_pulseWidth(degrees):
     print"\nGenerated servo motor angle: ", degrees, "degrees"
 
     # PulseWidth = [(Max Pulse Width - Min Pulse Width) / 181] + Pulse Width @ 0 degrees
-    pulseWidth = ((degrees * (1000/181)) + 1500)
+    pulseWidth = ((degrees * ((MAX_PULSEWIDTH-MIN_PULSEWIDTH)/ANGLE_OF_ROTATION)) + PULSEWIDTH_0DEGREES)
     # print"Servo motor pulse width: ", pulseWidth
     move_to_angle(pulseWidth)
 
@@ -51,13 +57,17 @@ def my_callback(GPIO, level, tick):
     global current_state
     current_state = 'RANDOM_MOVE'
 
+cb = pi.callback(BUTTON, pigpio.FALLING_EDGE, my_callback)
+
 # ------------------ MAIN LOOP --------------------
 try:
     while True:
-        cb = pi.callback(BUTTON, pigpio.FALLING_EDGE, my_callback)
+        # if 'WAIT_FOR_BUTTON', should do nothing.
+
+        # if 'RANDOM_MOVE', start the following
         if (current_state == 'RANDOM_MOVE'):
             # global degrees
-            degrees = random.randint(-90, 90)
+            degrees = random.randint(-90, 90) # generate random number from -90 to 90, representing min and max angle of servo
             # print"Generated servo motor angle: ", degrees, "degrees"
             to_pulseWidth(degrees)
             current_state = 'WAIT_FOR_BUTTON'
